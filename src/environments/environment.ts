d2cee65a2436612bba8c44c0849fc04e2225ee5f
Malthe Thomsen <malthe3@outlook.com>
2023-10-13 13:03:56 +0200

    tt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '1446fd860b344643fce09d3f96debc19', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*

db0daf1d5994ea8e34740dcc6a4819a9ae118547
Asland3 <malthe3@outlook.com>
2023-11-20 09:39:54 +0100

    Add API key and base URL to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '1446fd860b344643fce09d3f96debc19', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
